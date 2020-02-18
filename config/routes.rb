e09e6039221675d59a1dee05790113feaeab6bf1
Matheus Penchel <matheus.penchel@gmail.com>
2020-02-18 00:31:07 -0300

    Livecode: fake db AirBnB

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'flats', to: 'flats#index', as: :flats
+  get 'flats/:id', to: 'flats#show', as: :flat
+  root to: 'flats#index'
+end
